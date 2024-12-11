065b6f476346ad8d188b96f8941d603903567728
CaioBertoldo <caio23bezerra@gmail.com>
2024-12-10 22:14:55 -0400

    html e css

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Elementos
+const notesContainer = document.querySelector("#notes-container");
+const noteInput = document.querySelector("#note-content");
+const addNoteBtn = document.querySelector(".add-note")
+
+// Funções
+function addNote() {
+    console.log('teste')
+}
+
+// Eventos
+addNoteBtn.addEventListener("click", () => addNote())
